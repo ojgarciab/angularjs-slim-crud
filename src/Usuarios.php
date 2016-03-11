59fcd4f3727237b4d7f0ffbf62c6eda786832a42
oscargarcia <oscar.c9.io@linaresdigital.com>
2016-03-11 18:23:35 +0000

    Mejora #4: Refactorizado deleteUsuario, readUsuario y updateUsuario

--- src/Usuarios.php
+++ src/Usuarios.php
@@ -19,74 +19,111 @@ class Usuarios
         \Psr\Http\Message\ServerRequestInterface $peticion,
         \Psr\Http\Message\ResponseInterface $respuesta
     ) {
+        /* Configuramos el tipo MIME correcto para una salida JSON */
         $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
         $body = $respuesta->getBody();
         try {
+            /* Obtenemos el listado de ususarios */
             $conexion = \miPDO\Conexion::obtenerPDO();
-            $resultado = $conexion->query('SELECT * FROM usuarios');
-            $body->write(json_encode([
-            'error' => false,
-            'usuarios' => $resultado->fetchAll(\PDO::FETCH_OBJ),
-            ]));
+            $resultado = $conexion->query(
+                'SELECT * FROM usuarios'
+            );
+            $body->write(
+                json_encode([
+                    'error' => false,
+                    'usuarios' => $resultado->fetchAll(\PDO::FETCH_OBJ),
+                ])
+            );
         } catch (\PDOException $e) {
-            $body->write(json_encode([
-                'error' => true,
-                'mensaje' => $e->getMessage(),
-            ]));
+            /* En caso de error enviamos el mensaje */
+            $body->write(
+                json_encode([
+                    'error' => true,
+                    'mensaje' => $e->getMessage(),
+                ])
+            );
         }
         return $respuesta;
     }
     
-    static public function readUsuario($peticion, $respuesta, $argumentos) {
-    // Obtener un usuario por su identificador
-    $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
-    $body = $respuesta->getBody();
-    try {
-      $conexion = \miPDO\Conexion::obtenerPDO();
-      $consulta = $conexion->prepare('SELECT * FROM usuarios WHERE id = :id');
-      $consulta->bindValue(':id', $argumentos['id'], \PDO::PARAM_INT);
-      $consulta->execute();
-      $body->write(json_encode([
-        'error' => false,
-        'usuario' => $consulta->fetch(\PDO::FETCH_OBJ),
-      ]));
-    } catch (\PDOException $e) {
-      $body->write(json_encode([
-        'error' => true,
-        'mensaje' => $e->getMessage(),
-      ]));
-    }
-    return $respuesta;
-    }
-    
-    static public function updateUsuario($id) {
-    // TODO: Actualizar un usuario por su identificador usando los datos enviados en el formulario
-    }
-    
-    static public function deleteUsuario($peticion, $respuesta, $argumentos) {
-    // Borrar un usuario por su identificador
-    $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
-    $body = $respuesta->getBody();
-    try {
-      $conexion = \miPDO\Conexion::obtenerPDO();
-      $consulta = $conexion->prepare('DELETE FROM usuarios WHERE id = :id');
-      $consulta->bindValue(':id', $argumentos['id'], \PDO::PARAM_INT);
-      $consulta->execute();
-      $body->write(json_encode([
-        'error' => false,
-        'borrados' => $consulta->rowCount(),
-      ]));
-    } catch (\PDOException $e) {
-      $body->write(json_encode([
-        'error' => true,
-        'mensaje' => $e->getMessage(),
-      ]));
-    }
-    return $respuesta;
+    static public function readUsuario(
+        \Psr\Http\Message\ServerRequestInterface $peticion,
+        \Psr\Http\Message\ResponseInterface $respuesta,
+        $argumentos
+    ) {
+        /* Configuramos el tipo MIME correcto para una salida JSON */
+        $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
+        $body = $respuesta->getBody();
+        try {
+            /* Obtenemos el usuario cuyo identificador ha sido pasado como parámetro */
+            $conexion = \miPDO\Conexion::obtenerPDO();
+            $consulta = $conexion->prepare(
+                'SELECT * FROM usuarios WHERE id = :id'
+            );
+            $consulta->bindValue(':id', $argumentos['id'], \PDO::PARAM_INT);
+            $consulta->execute();
+            $body->write(
+                json_encode([
+                    'error' => false,
+                    'usuario' => $consulta->fetch(\PDO::FETCH_OBJ),
+                ])
+            );
+        } catch (\PDOException $e) {
+            /* En caso de error enviamos el mensaje */
+            $body->write(
+                json_encode([
+                    'error' => true,
+                    'mensaje' => $e->getMessage(),
+                ])
+            );
+        }
+        return $respuesta;
     }
     
-    private function obtenerConexion() {
-    //  Establecer conexión a la base de datos
-    return \PDO\Conexion::obtenerPDO();
+    static public function updateUsuario(
+        \Psr\Http\Message\ServerRequestInterface $peticion,
+        \Psr\Http\Message\ResponseInterface $respuesta,
+        $argumentos
+    ) {
+        // TODO: Actualizar un usuario por su identificador usando los datos enviados en el formulario
+    }
+    
+    static public function deleteUsuario(
+        \Psr\Http\Message\ServerRequestInterface $peticion,
+        \Psr\Http\Message\ResponseInterface $respuesta,
+        $argumentos
+    ) {
+        $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
+        $body = $respuesta->getBody();
+        try {
+            /* Borramos el usuario con el identificador proporcionado */
+            $conexion = \miPDO\Conexion::obtenerPDO();
+            $consulta = $conexion->prepare(
+                'DELETE FROM usuarios WHERE id = :id'
+            );
+            $consulta->bindValue(':id', $argumentos['id'], \PDO::PARAM_INT);
+            $consulta->execute();
+            $body->write(
+                json_encode([
+                    'error' => false,
+                    'borrados' => $consulta->rowCount(),
+                ])
+            );
+        } catch (\PDOException $e) {
+            /* En caso de error enviamos el mensaje */
+            $body->write(
+                json_encode([
+                    'error' => true,
+                    'mensaje' => $e->getMessage(),
+                ])
+            );
+        }
+        return $respuesta;
+    }
+    
+    private function obtenerConexion()
+    {
+        //  Establecer conexión a la base de datos
+        return \PDO\Conexion::obtenerPDO();
     }
 }
