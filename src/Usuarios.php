006489fd39cd46307b38d8bdca075dab1e1010ee
oscargarcia <oscar.c9.io@linaresdigital.com>
2016-03-11 18:05:33 +0000

    Mejora #4: Refactorizando readUsuarios

--- src/Usuarios.php
+++ src/Usuarios.php
@@ -1,37 +1,43 @@
 <?php
 namespace ASC;
 
-class Usuarios {
-  private $conexion = false;
-
-  public function testIt($bool = true) {
-    return $bool;
-  }
-
-  public function createUsuario() {
-    // TODO: Crear usuario nuevo con los datos enviados en el formulario
-  }
-
-  static public function readUsuarios($peticion, $respuesta) {
-    $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
-    $body = $respuesta->getBody();
-    try {
-      $conexion = \miPDO\Conexion::obtenerPDO();
-      $resultado = $conexion->query('SELECT * FROM usuarios');
-      $body->write(json_encode([
-        'error' => false,
-        'usuarios' => $resultado->fetchAll(\PDO::FETCH_OBJ),
-      ]));
-    } catch (\PDOException $e) {
-      $body->write(json_encode([
-        'error' => true,
-        'mensaje' => $e->getMessage(),
-      ]));
+class Usuarios
+{
+    private $conexion = false;
+    
+    public function testIt($bool = true)
+    {
+        return $bool;
     }
-    return $respuesta;
-  }
-
-  static public function readUsuario($peticion, $respuesta, $argumentos) {
+    
+    public function createUsuario()
+    {
+        // TODO: Crear usuario nuevo con los datos enviados en el formulario
+    }
+    
+    static public function readUsuarios(
+        \Psr\Http\Message\ServerRequestInterface $peticion,
+        \Psr\Http\Message\ResponseInterface $respuesta
+    ) {
+        $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
+        $body = $respuesta->getBody();
+        try {
+            $conexion = \miPDO\Conexion::obtenerPDO();
+            $resultado = $conexion->query('SELECT * FROM usuarios');
+            $body->write(json_encode([
+            'error' => false,
+            'usuarios' => $resultado->fetchAll(\PDO::FETCH_OBJ),
+            ]));
+        } catch (\PDOException $e) {
+            $body->write(json_encode([
+                'error' => true,
+                'mensaje' => $e->getMessage(),
+            ]));
+        }
+        return $respuesta;
+    }
+    
+    static public function readUsuario($peticion, $respuesta, $argumentos) {
     // Obtener un usuario por su identificador
     $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
     $body = $respuesta->getBody();
@@ -51,13 +57,13 @@ class Usuarios {
       ]));
     }
     return $respuesta;
-  }
-
-  static public function updateUsuario($id) {
+    }
+    
+    static public function updateUsuario($id) {
     // TODO: Actualizar un usuario por su identificador usando los datos enviados en el formulario
-  }
-
-  static public function deleteUsuario($peticion, $respuesta, $argumentos) {
+    }
+    
+    static public function deleteUsuario($peticion, $respuesta, $argumentos) {
     // Borrar un usuario por su identificador
     $respuesta = $respuesta->withHeader('Content-Type', 'application/json');
     $body = $respuesta->getBody();
@@ -77,10 +83,10 @@ class Usuarios {
       ]));
     }
     return $respuesta;
-  }
-
-  private function obtenerConexion() {
+    }
+    
+    private function obtenerConexion() {
     //  Establecer conexión a la base de datos
     return \PDO\Conexion::obtenerPDO();
-  }
+    }
 }
