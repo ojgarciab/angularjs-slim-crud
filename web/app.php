13100addbfc9de214a6bdea494e5d7259fb21a1c
oscargarcia <oscar.c9.io@linaresdigital.com>
2016-03-15 21:12:38 +0000

    Mejora #19: Pasar de clases estáticas a clases normales

--- web/app.php
+++ web/app.php
@@ -4,11 +4,11 @@ require '../vendor/autoload.php';
 $app = new \Slim\App();
 
 /* Rutas a los servicios RESTful */
-$app->get('/usuarios', '\ASC\Usuarios::readUsuarios');
-$app->put('/usuarios', '\ASC\Usuarios::createUsuario');
-$app->get('/usuarios/{id}', '\ASC\Usuarios::readUsuario');
-$app->put('/usuarios/{id}', '\ASC\Usuarios::updateUsuario');
-$app->delete('/usuarios/{id}', '\ASC\Usuarios::deleteUsuario');
+$app->get('/usuarios', '\ASC\Usuarios:readUsuarios');
+$app->put('/usuarios', '\ASC\Usuarios:createUsuario');
+$app->get('/usuarios/{id}', '\ASC\Usuarios:readUsuario');
+$app->put('/usuarios/{id}', '\ASC\Usuarios:updateUsuario');
+$app->delete('/usuarios/{id}', '\ASC\Usuarios:deleteUsuario');
 
 
 $app->run();
