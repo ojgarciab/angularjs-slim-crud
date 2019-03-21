a2627291be883d5148ac423bd9d9d62761b30668
Oscar Garcia <oscar.github@linaresdigital.com>
2019-03-21 16:02:57 +0100

    Mejora #32: Encapsulada la lógica de la aplicación en su propia clase

---
+++ src/Api.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ASC;
+
+class Api
+{
+    private $app;
+
+    public function __construct() {
+        $this->app = new \Slim\App;
+        /* Rutas a los servicios RESTful */
+        $this->app->get('/usuarios', '\ASC\Usuarios:readUsuarios');
+        $this->app->put('/usuarios', '\ASC\Usuarios:createUsuario');
+        $this->app->get('/usuarios/{id}', '\ASC\Usuarios:readUsuario');
+        $this->app->put('/usuarios/{id}', '\ASC\Usuarios:updateUsuario');
+        $this->app->delete('/usuarios/{id}', '\ASC\Usuarios:deleteUsuario');
+    }
+
+    public function getApp() {
+        return $this->app;
+    }
+}
--- web/app.php
+++ web/app.php
@@ -2,13 +2,6 @@
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App();
+$api = new \ASC\Api();
 
-/* Rutas a los servicios RESTful */
-$app->get('/usuarios', '\ASC\Usuarios:readUsuarios');
-$app->put('/usuarios', '\ASC\Usuarios:createUsuario');
-$app->get('/usuarios/{id}', '\ASC\Usuarios:readUsuario');
-$app->put('/usuarios/{id}', '\ASC\Usuarios:updateUsuario');
-$app->delete('/usuarios/{id}', '\ASC\Usuarios:deleteUsuario');
-
-$app->run();
+$api->getApp()->run();
